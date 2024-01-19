e4ae2cf7bd33d59cc1d6402017c338d53fd1948e
ghizlanejadraoui <gjadraoui@gmail.com>
2024-01-19 14:39:36 +0100

    add section carousel

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+// let myAllbuttons = document.querySelector(`.btn1`);
+// let Mydiv = doucument.querySelector(`.divforimg2`)
+// myAllbuttons.addEventListener(`click`,()=>{
+// Mydiv.toggle(".clickdiv");
+//     })
+
